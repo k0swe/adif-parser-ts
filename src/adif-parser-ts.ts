52eb7d9a96737be694df25c00388b5567aca2ca6
Chris Keller <xylo04@gmail.com>
2020-10-21 20:43:20 -0600

    First pass at ADIf parsing

--- src/adif-parser-ts.ts
+++ src/adif-parser-ts.ts
@@ -1,6 +1,52 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+export default class AdifParser {
 
+  static parseAdi(adi: string): object {
+    return new AdifParser(adi).parseTopLevel();
+  }
+
+  private cursor = 0;
+
+  private constructor(private readonly adi: string) {
+  }
+
+
+  parseTopLevel(): object {
+    const parsed: { [key: string]: any } = {};
+    // TODO: Header
+    // QSO Records
+    const records = new Array<object>();
+    while (this.cursor < this.adi.length) {
+      records.push(this.parseRecord());
+    }
+    parsed["records"] = records;
+    return parsed;
+  }
+
+  private parseRecord(): { [field: string]: string } {
+    const record: { [field: string]: string } = {};
+    while (this.cursor < this.adi.length) {
+      const endOfRecord = this.parseTagValue(record);
+      if (endOfRecord) {
+        break;
+      }
+    }
+    return record;
+  }
+
+  private parseTagValue(record: { [p: string]: string }): boolean {
+    const startTag = this.adi.indexOf('<', this.cursor);
+    const endTag = this.adi.indexOf('>', startTag);
+    const tagParts = this.adi.substring(startTag + 1, endTag).split(":");
+    if (tagParts[0].toLowerCase() === 'eor' || tagParts[0].toLowerCase() === 'eoh') {
+      this.cursor = endTag + 1;
+      return true;
+    } else if (tagParts.length < 2) {
+      throw new Error();
+    }
+    const fieldName = tagParts[0].toLowerCase();
+    const width = +tagParts[1];
+    record[fieldName] = this.adi.substr(endTag + 1, width);
+    this.cursor = endTag + 1 + width;
+    return false;
+  }
 }
--- test/adif-parser-ts.test.ts
+++ test/adif-parser-ts.test.ts
@@ -1,14 +1,23 @@
-import DummyClass from "../src/adif-parser-ts"
+import AdifParser from "../src/adif-parser-ts"
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
+describe("AdifParser test", () => {
   it("works if true is truthy", () => {
     expect(true).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it("should parse a basic ADI", () => {
+    expect(AdifParser.parseAdi(`<CALL:6>J72IMS <QSO_DATE:8>20200328 <eor>
+<CALL:4>KK9A <QSO_DATE:8>20200329 <eor>`))
+      .toEqual({
+        "records": [
+          {
+            "call": "J72IMS",
+            "qso_date": "20200328"
+          }, {
+            "call": "KK9A",
+            "qso_date": "20200329"
+          }
+        ]
+      });
   })
 })
